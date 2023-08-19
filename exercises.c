35a17be3689460f830e9b2fdb811c734e1b38e64
Replit user <>
2023-08-19 01:34:44 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -9,7 +9,6 @@ Ejercicio 1: Encuentra el Elemento Mayor
 Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
-//int findMax(int arr[], int size) { return 0; }
 int findMax(int arr[], int size)
 {
     
@@ -28,7 +27,6 @@ Ejercicio 2: Invertir un Arreglo
 Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
-//void reverseArray(int arr[], int size) {}
 void reverseArray(int arr[], int size)
 {
   int i;
@@ -45,7 +43,6 @@ Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
-//int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
 int *filterEvenNumbers(int arr[], int size, int *sizeNuevo)
 {
     int pares = 0;
@@ -74,8 +71,6 @@ Descripción: Escribe una función que tome dos arreglos
 ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
-//void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-//                       int result[]) {}
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[]) {
     int i = 0, j = 0, k = 0;
 
@@ -101,7 +96,33 @@ Descripción: Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int checkSorted(int arr[], int size) { return -2; }
+int checkSorted(int arr[], int size)
+{
+  int ascend = 1;
+  int descend = 1;
+  
+  for (int i=1;i<size;i++)
+  {
+        if (arr[i]<arr[i-1]){
+        ascend=0;
+        }
+        if (arr[i]>arr[i-1]){
+        descend=0;
+        }
+    }
+  if (ascend){
+    return 1;
+  } else if (descend){
+    return -1;
+  } else{
+    return 0;
+  }
+}
+
+
+
+
+
 
 /*
 Ejercicio 6: Información de una Biblioteca
